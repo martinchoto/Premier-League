c241a9a0d3e2cb61371e6a7d85ab5c8389a73971
martinchoto <martin_stalev03@abv.bg>
2026-01-06 16:03:24 +0200

    java api init

---
+++ premier-league.api/src/test/java/io/github/martinchoto/premier_league/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.martinchoto.premier_league.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
